673bac01e9e14e9c2a6f290538e173c395147d21
satyampandeyhcc <satyampandeyhcc@gmail.com>
2023-09-15 16:03:29 +0530

    commit

--- Fronted/src/App.js
+++ Fronted/src/App.js
@@ -1,23 +1,40 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch, Link, Redirect } from "react-router-dom";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 import GetBooking from "./components/GetBookings";
 import GetFlights from "./components/GetFlights";
-
-import Evaluator from './testing/evaluator';
+import Evaluator from "./testing/evaluator";
 import "./App.css";
 
-class AppComp extends Component {
-  render() {
-    return (
-      <Router>
-        <div>
-          <Evaluator></Evaluator>
-          <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-3">
-            <span className="navbar-brand font-weight-bolder"><img alt="navbar brand logo" src="assets/logo.png" height="30vw" width="30vw" className="mr-2"/> Fly High</span>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="collapsibleNavbar">
+function AppComp() {
+  return (
+    <Router>
+      <div>
+        <Evaluator></Evaluator>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-3">
+          <span className="navbar-brand font-weight-bolder">
+            <img
+              alt="navbar brand logo"
+              src="assets/logo.png"
+              height="30vw"
+              width="30vw"
+              className="mr-2"
+            />{" "}
+            Fly High
+          </span>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#collapsibleNavbar"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="collapsibleNavbar">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item mr-2">
                 <Link className="nav-link font-weight-bold" to="/bookFlight">
@@ -30,19 +47,15 @@ class AppComp extends Component {
                 </Link>
               </li>
             </ul>
-            </div>
-          </nav>
-                   
-          <Switch>
-          <Route exact path="/" render={() =>(<Redirect to="/bookFlight"/>)} />
-            <Route exact path="/bookFlight" component={GetFlights} />
-            <Route exact path="/viewBooking" component={GetBooking} />
-          </Switch>
           </div>
-        
-      </Router>
-    );
-  }
+        </nav>
+
+        <Route exact path="/" render={() => <Redirect to="/bookFlight" />} />
+        <Route exact path="/bookFlight" component={GetFlights} />
+        <Route exact path="/viewBooking" component={GetBooking} />
+      </div>
+    </Router>
+  );
 }
 
 export default AppComp;
